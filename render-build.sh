1257bf9b60b4c36b5856060304426e8410ebdb1c
eldeivis130 <davidalmara8@gmail.com>
2025-12-04 01:56:37 -0600

    Add PHP version file and fix Composer installation

--- render-build.sh
+++ render-build.sh
@@ -2,8 +2,27 @@
 # exit on error
 set -o errexit
 
+# Instalar Composer si no existe
+if ! command -v composer &> /dev/null; then
+    echo "Installing Composer..."
+    EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+    
+    if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]; then
+        >&2 echo 'ERROR: Invalid installer checksum'
+        rm composer-setup.php
+        exit 1
+    fi
+    
+    php composer-setup.php --quiet
+    rm composer-setup.php
+    export PATH="$PATH:$PWD"
+    alias composer="php composer.phar"
+fi
+
 # Instalar dependencias de Composer
-composer install --no-dev --optimize-autoloader
+php composer.phar install --no-dev --optimize-autoloader || composer install --no-dev --optimize-autoloader
 
 # Instalar dependencias de Node
 npm install
